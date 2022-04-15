b4205f044cbe05d74b3d9670391d301929f37be1
Ravenclawldz <ravenclaw.ldz@gmail.com>
2022-04-15 00:24:54 -0400

    Initial commit

---
+++ bot/constants.py
@@ -0,0 +1,20 @@
+# Admnistradores
+# Para añadir más separalos por comas
+administradores = [
+    1037377870, 1618347551
+    ]
+
+# Canal a donde se envia la plantilla
+# Se debe incluir el ' - '
+CHANNEL = -1001181743489
+
+# Estados ConversationHandler
+PHOTO = 0
+NOMBRE = 1
+CALIDAD_VIDEO = 2
+SIZE = 3
+CAPITULOS = 4
+PARTES = 5
+TEMPORADA = 6
+ARGUMENTO = 7
+SEND = 8
